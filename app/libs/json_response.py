ba48695cb76583681fa7a810536de5249ac5b739
fanhaipeng <938376959@qq.com>
2017-03-04 14:37:42 +0800

    first

---
+++ app/libs/json_response.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from flask import jsonify
+from  werkzeug.wrappers import Response
+
+
+
+class JSONResponse(Response):
+    @classmethod
+    def force_type(cls, rv, environ=None):
+        if isinstance(rv,dict):
+            rv=jsonify(rv)
+        return super(JSONResponse,cls).force_type(rv,environ)
+
+
